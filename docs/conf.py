005cefcee636dac139bf75de6cbd8a664991960e
René Sass <mail@renesass.de>
2022-02-09 12:40:20 +0100

    Changed github url

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,11 @@ import src
 import automl_sphinx_theme
 
 
-CUSTOM_OPTIONS = {"html_theme_options": {"github_url": "https://renesass.de"}}
+CUSTOM_OPTIONS = {
+    "html_theme_options": {
+        "github_url": "https://automl.github.io/automl_sphinx_theme/main"
+    }
+}
 
 
 # Import conf.py from the automl theme
